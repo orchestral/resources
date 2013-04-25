ef282af90b3192f6453ea6b553591dc023e2333e
crynobone <crynobone@gmail.com>
2013-04-25 11:32:21 +0800

    Add deferred service provider.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Orchestra/Resources/ResourcesServiceProvider.php
@@ -0,0 +1,36 @@
+<?php namespace Orchestra\Resources;
+
+use Illuminate\Support\ServiceProvider;
+
+class ResourcesServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = true;
+	
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register() 
+	{
+		$this->app['orchestra.resources'] = $this->app->share(function ($app)
+		{
+			return new Environment($app);
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('orchestra.resources');
+	}
+}
